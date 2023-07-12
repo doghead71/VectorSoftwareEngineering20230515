955eb3b4e0917aad85bb7a891f1a1d85e4139324
Mentald1 <110870582+Mentald1@users.noreply.github.com>
2023-07-12 18:28:29 -0500

    Create PasswordspaceValidator.java

---
+++ code/euroFootball/src/main/java/com/mycompany/eurofootball/validators/PasswordspaceValidator.java
@@ -0,0 +1,12 @@
+package com.mycompany.eurofootball.validators;
+/**
+*
+* @author Johnross Bailey
+*/
+public interface PasswordspaceValidator {
+   
+    
+    boolean validatePassword (String password);
+ 
+    
+}
